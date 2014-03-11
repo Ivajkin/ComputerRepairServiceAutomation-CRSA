2260b37d9a7a62c20f938508bd9e3273edfa12cd
Ivaykin Timofey <ivaykin@live.com>
2014-03-11 16:23:15 +1100

    All SQL tables created

--- createDB.sql
+++ createDB.sql
@@ -83,6 +83,136 @@ INSERT INTO `warehouse_item` (`invoice_number`, `hardware_id`, `item_count`, `wa
 ('es0215', 1, 15, '5 лет, с 30 мая 2012');
 
 
+--
+--	Таблица производителей -> manufacturer
+--		Код производителя	-> id
+--		Наименование производителя -> name
+create table if not exists `manufacturer` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `manufacturer` (`name`) VALUES
+('ASUS');
+
+
+--
+--	Таблица неисправностей -> fault
+--		Код неисправности
+--		Наименование неисправности
+create table if not exists `fault` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(200) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `fault` (`name`) VALUES
+('Разрыв шлейфов');
+
+
+--
+--	Внешний вид -> appearance
+--		Код внешнего вида
+--		Наименование внешнего вида
+create table if not exists `appearance` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `appearance` (`name`) VALUES
+('В отличном состоянии');
+
+--
+--	Комплектность -> completeness
+--		Код комплектности
+--		Наименование комплектности
+create table if not exists `completeness` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `completeness` (`name`) VALUES
+('Полная комплектность');
+
+--
+--	Источник -> source
+--		Код источника
+--		Наименование источника
+create table if not exists `source` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `source` (`name`) VALUES
+('Заявка клиента');
+
+--
+--	Выполненные работы -> task
+--		Код выполненной работы
+--		Наименование выполненной работы
+create table if not exists `task` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `task` (`name`) VALUES
+('Замена шлейфа');
+
+
+--
+--	Статус заявки -> request_status
+--		Идентификатор
+--		Наименование статуса
+create table if not exists `request_status` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `request_status` (`name`) VALUES
+('В работе'),
+('В ожидании'),
+('На согласовании'),
+('Готов');
+
+--
+--	Роль сотрудника в системе -> role
+--		Идентификатор
+--		Наименование роли
+create table if not exists `role` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'Администратор'),
+(2, 'Приемщик');
+
+
+--	Сотрудники -> employee
+--		Код сотрудника -> id
+--		ФИО -> name
+--    email для сброса пароля -> email
+--		Логин -> login
+--		Пароль -> password_hash (sha-256(login+pass))
+--		Процент оплаты -> fee
+--		Роль -> role_id
+create table if not exists `employee` (
+  `id` int not null AUTO_INCREMENT unique primary key,
+  `name` varchar(100) CHARACTER SET utf8 not null unique,
+  `email` varchar(100) CHARACTER SET utf8 not null unique,
+  `login` varchar(100) CHARACTER SET utf8 not null unique,
+  `password_hash` varchar(256) CHARACTER SET utf8 not null,
+  `fee` integer default 0, CONSTRAINT percent_value CHECK (fee >= 0 and fee <=100),
+  `role_id` integer not null references role(id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+-- login: milkshake32 pass: milkshake32
+-- login: avangebit pass: 123456
+INSERT INTO `employee` (`name`, `login`, `password_hash`, `email`, `fee`, `role_id`) VALUES
+('Иванов Иван Гольцман', 'milkshake32', 'aec6c35c00a0cadc8bf97f4d94e55e9a547bfb8afcf86e2c0602c6d865f84c5d', 'i.golz@mail.ru', 10, 2),
+('Алексей Верещагин', 'avangebit', '27f5365e3d7609197e381253b6e361e29a53732b53eb2be5884594631378c2cf', 'avangebit@gmail.com', 10, 1);
+
+
+
 
 --	Таблица заявок -> Request -> request
 --		Номер заявки -> request Number? (string or int??)
@@ -121,52 +251,31 @@ create table if not exists `request` (
   `phone` varchar(20),
   `address` varchar(300) CHARACTER SET utf8 default null,
   `customer_name` varchar(300) CHARACTER SET utf8 not null,
-  `source_id` integer not null references source(id),  
+  `source_id` integer not null references source(id),
   `date_of_call` varchar(20),
   `note` varchar(200),
   `approximate_cost` int default null,
   `prepayment` int default null,
   `acceptor_id` integer not null references employee(id),
   `responsible_id` integer not null references employee(id),
-  `date_of_reciept` varchar(20) not null,
+  `date_of_receipt` varchar(20) not null,
   `date_of_issue` varchar(20),
   `amount` int,
-  `method_of_peyment` varchar(100),
-  `request_status_id` integer not null references status(id),
-  `completed_works_id` integer not null references completed_works(id),
-  `parts_installed_id` integer not null references goods(id)
+  `method_of_payment` varchar(100),
+  `request_status_id` integer not null references request_status(id),
+  `completed_works_id` integer not null references task(id),
+  `parts_installed_id` integer not null references hardware(id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+
+INSERT INTO `request` (`hardware_id`, `manufacturer_id`, `model`, `serial_number`, `fault_id`, `appearance_id`, `completeness_id`, `phone`, `address`, `customer_name`, `source_id`, `date_of_call`, `note`, `approximate_cost`, `prepayment`, `acceptor_id`, `responsible_id`, `date_of_receipt`, `date_of_issue`, `amount`, `method_of_payment`, `request_status_id`, `completed_works_id`, `parts_installed_id`) VALUES
+(1, 1, `model`, `serial_number`, 1, 1, 1, `phone`, `address`, `customer_name`, 1, `date_of_call`, `note`, 123, 55, 1, 1, `date_of_receipt`, `date_of_issue`, 12, `method_of_payment`, 1, 1, 1);
 
 -- ------------------------------------------------------------------
 
-
---		
---	Таблица производителей
---		Код производителя	
---		Наименование производителя	
---		
---	Таблица неисправностей
---		Код неисправности	
---		Наименование неисправности	
---		
---	Внешний вид
---		Код внешнего вида	
---		Наименование внешнего вида	
---		
---	Комплектность
---		Код комплектности	
---		Наименование комплектности	
---		
---	Источник
---		Код источника	
---		Наименование источника	
---		
---	Выполненные работы
---		Код выполненной работы	
---		Наименование выполненной работы	
 --
 
---	Товар
+--	Товар  - ?
 --		Код товара	
 --		Название товара	
 --		Код категория	
@@ -182,14 +291,7 @@ create table if not exists `request` (
 --		Код товара	
 --		Номер накладной	
 --		Номер серийника	
---		
---	Сотрудники
---		Код сотрудника	
---		ФИО	
---		Логин	
---		Пароль	
---		Процент оплаты	
---		Роль	
+--
 --		
 --	Прейскурант работ
 --		Код работы	
