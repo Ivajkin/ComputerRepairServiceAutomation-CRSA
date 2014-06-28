c1cdd5a10862e09eb3fc116a8247364e8f72e146
Ivaykin Timofey <ivaykin@live.com>
2014-06-28 23:40:47 +1100

    Создано меню модуля отчетов и заголовок для отчета о движении денежных средств

--- src/main/webapp/js/reports.js
+++ src/main/webapp/js/reports.js
@@ -12,11 +12,47 @@ function openReportsModule() {
 
     $('#reportsModuleContainer').show();
 
-    $('#reportsModuleContainer').html(
+
+
+    function showCashReport() {
+        var report =
         '<table>' +
+            '<tr>' +
+                '<th>дата и время</th>' +
+                '<th>наименование товара</th>' +
+                '<th>сотрудник</th>' +
+                '<th>цена</th>' +
+                '<th>остаток</th>' +
+            '</tr>' +
             '<tr>' +
                 '<td>123</td>' +
                 '<td>321</td>' +
+                '<td>321</td>' +
+                '<td>321</td>' +
+                '<td>321</td>' +
             '</tr>' +
-        '</table>');
+        '</table>' +
+            '<a class="backButton">Назад</a>' +
+            '</br>' +
+            '</br>' +
+            '</br>' +
+            '</br>';
+        $('#reportsModuleContainer').html(report);
+        $('.backButton').click(showMenu);
+    }
+
+    function showMenu() {
+        var reportList =
+            "<ul>" +
+                "<li>Зарплата мастерам</li>" +
+                "<li id=\"cashReport\">Движение денежных средств</li>" +
+                "<li>Остатки на складе</li>" +
+                "<li>Списание со склада</li>" +
+                "<li>Принятие на склад</li>" +
+                "<li>Валовая прибыль</li>" +
+            "</ul>";
+        $('#reportsModuleContainer').html(reportList);
+        $('#cashReport').click(showCashReport);
+    }
+    showMenu();
 }
