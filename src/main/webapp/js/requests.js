24d429c700998a551011c2b5a383dd6b27f3f680
Ivaykin Timofey <ivaykin@live.com>
2014-08-21 11:51:10 +1100

    При открытии формы создания заявки делаем ("дату выдачи" и "сумму работы") неактивной
     При открытии формы изменения заявки если статус не выдан/готов делаем ("дату выдачи" и "сумму работы") неактивной
    
     При изменении статуса заявки если статус не выдан/готов делаем ("дату выдачи" и "сумму работы") неактивной

--- src/main/webapp/js/requests.js
+++ src/main/webapp/js/requests.js
@@ -46,8 +46,6 @@ function openRequests() {
              //   deleteAction: '/requests/delete'
             },
 			fields: {
-                // TODO: Добавить все поля и настроить для каждого свойства (в первую очередь для тех, что выбираем из списка)
-                // TODO: Применить дочерние таблицы (CHILD TABLE), http://www.jtable.org/demo/masterchild
                 req_num_id: {
                     title: 'Номер заявки',
                     key: true,
@@ -304,23 +302,29 @@ function openRequests() {
 
         $('.ui-dialog-buttonpane.ui-widget-content').append('<button id="btnPrint" style="position:absolute; right:185px;" onclick="onPrint();">Печать</button>');
 
+
+        function toggleDoneState() {
+            var status_id = $('#Edit-request_status_id').find('option:selected').val();
+            var status_name = $('#Edit-request_status_id').find('option:selected').text();
+            var disable_status = true;
+            if(status_id === 3) {
+                assert(status_name === 'Выдан/Выполнен', 'Значение поля статуса заявки (' + status_id + ') указывает на статус "Выдан/Выполнен" но имеет значение "' + status_name + '". Возможно порядок статусов перепутан.');
+                disable_status = false;
+            } else if(status_id === 2) {
+                assert(status_name === 'Готов', 'Значение поля статуса заявки (' + status_id + ') указывает на статус "Готов" но имеет значение "' + status_name + '". Возможно порядок статусов перепутан.');
+                disable_status = false;
+            }
+
+            $('#Edit-date_of_issue').prop( "disabled", disable_status);
+            $('#Edit-amount').prop( "disabled", disable_status);
+        }
+
+
         $('.jtable-toolbar-item-add-record').click(function() {
-                $('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons.ui-draggable.ui-resizable')
-                    .css('width', '300px');
-            $('#Edit-request_status_id').change(function(){
-                if($('#Edit-request_status_id').val() == 1)
-                {
-                    $('#Edit-date_of_issue').attr('disabled', 'disabled');
-                    $('#Edit-amount').attr('disabled', 'disabled');
-                    $('#Edit-task').attr('disabled', 'disabled');
-                }
-                else
-                {
-                    $('#Edit-date_of_issue').removeAttr('disabled');
-                    $('#Edit-amount').removeAttr('disabled');
-                    $('#Edit-task').removeAttr('disabled');
-                }
-            });
+            $('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons.ui-draggable.ui-resizable')
+                .css('width', '400px');
+            toggleDoneState();
+            $('#Edit-request_status_id').change(toggleDoneState);
         });
 
         $('#requestsTableContainer').jtable('load');
