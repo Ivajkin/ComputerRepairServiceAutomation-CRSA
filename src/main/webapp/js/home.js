3ae849f795aaf9979f8021b19f56b5b2868e3bfa
Igonina Galina <igonina2101@gmail.com>
2015-03-12 13:22:09 +1000

    AJAX and unhandled error handling

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -3,6 +3,45 @@
  * Date: 2/18/14
  */
 
+// Substring text with html tags
+function htmlSubstring(s, n) {
+    var m, r = /<([^>\s]*)[^>]*>/g,
+        stack = [],
+        lasti = 0,
+        result = '';
+
+    //for each tag, while we don't have enough characters
+    while ((m = r.exec(s)) && n) {
+        //get the text substring between the last tag and this one
+        var temp = s.substring(lasti, m.index).substr(0, n);
+        //append to the result and count the number of characters added
+        result += temp;
+        n -= temp.length;
+        lasti = r.lastIndex;
+
+        if (n) {
+            result += m[0];
+            if (m[1].indexOf('/') === 0) {
+                //if this is a closing tag, than pop the stack (does not account for bad html)
+                stack.pop();
+            } else if (m[1].lastIndexOf('/') !== m[1].length - 1) {
+                //if this is not a self closing tag than push it in the stack
+                stack.push(m[1]);
+            }
+        }
+    }
+
+    //add the remainder of the string, if needed (there are no more tags in here)
+    result += s.substr(lasti, n);
+
+    //fix the unclosed tags
+    while (stack.length) {
+        result += '</' + stack.pop() + '>';
+    }
+
+    return result;
+
+}
 
 /* Message system */
 var messagebox = {
@@ -21,6 +60,12 @@ window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
     var errorMessage = "Необработанная ошибка: " + errorMsg + "<br/>Путь: "+url+"<br/>Номер строки: "+lineNumber;
     messagebox.error(errorMessage, "Необработанная ошибка");
 };
+$(document).ajaxError(function( event, jqxhr, settings, thrownError ) {
+    var errorMessage = thrownError
+        + '<br/>Error: ' + htmlSubstring(jqxhr.responseText, 199)+'...'
+        + '<br/>URL: ' + settings.url;
+    messagebox.error(errorMessage, "Ошибка AJAX");
+});
 (function initialiseMessageSystem() {
     toastr.options = {
         "closeButton": true,
